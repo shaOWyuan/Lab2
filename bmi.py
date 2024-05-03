6217c8c9cad42683b64642f0de3020f210947448
Shao Yuan <shaoyuan.23@ichat.sp.edu.sg>
2024-05-03 16:42:06 +0800

    Modified bmi.py

--- bmi.py
+++ bmi.py
@@ -8,5 +8,11 @@ def calculate_bmi(height, weight):
     bmi = weight / (height ** 2)
     
     print("BMI =", bmi)
+    if bmi<18.5:
+        print ("Under Weight")
+    elif bmi>25.0:
+        print ("Over Weight")
+    else:
+        print ("Normal Weight")
 
 calculate_bmi(weight='57', height='1.73')
